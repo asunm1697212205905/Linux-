58142b7f4841f9a6750f72157618d4a9b587f7cd
NIVETHA M <151614504+asunm1697212205905@users.noreply.github.com>
2026-03-25 16:10:23 +0530

    Add files via upload

---
+++ LINUX/b shell/circle.bash
@@ -0,0 +1,11 @@
+echo "Enter radius:"
+read r
+
+pi=3.14
+
+
+area=$(echo "$pi * $r * $r" | bc)
+perimeter=$(echo "2 * $pi * $r" | bc)
+
+echo "Area of circle: $area"
+echo "Perimeter of circle: $perimeter"
