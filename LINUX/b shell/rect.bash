14e8974894b973a0f900dc3493c4359c0b22fa6f
NIVETHA M <151614504+asunm1697212205905@users.noreply.github.com>
2026-03-25 12:41:32 +0530

    Add files via upload

---
+++ LINUX/b shell/rect.bash
@@ -0,0 +1,12 @@
+echo "Enter the length:"
+read length
+
+echo "Enter the width:"
+read width
+
+area=$((length * width)
+
+perimeter=$((2 * (length + width)))
+
+echo "Area of rectangle: $area"
+echo "Perimeter of rectangle: $perimeter"
